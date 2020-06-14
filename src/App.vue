436e0bedb1b65a3a2ab4ba4b32b141d443d12000
Roman Almazov <ralmaz.vue@ya.ru>
2020-06-14 18:28:41 +0300

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
 <template>
   <div>
-    {{ name }}
+    1
   </div>
 </template>
-
-<script>
-  export default {
-    mounted() {
-      setTimeout(() => {
-        this.name = '2342345324'
-      }, 3000)
-    },
-
-    computed: {
-      name:{
-        get() {
-          return this.$store.state.name
-        },
-
-        set(value) {
-          this.$store.commit('changeName', value)
-        }
-      }
-    }
-  }
-</script>
--- src/Evan.js
+++
@@ -1,5 +0,0 @@
-export default {
-  tascan: '4e956874 thp0ernmgnknxdfjkg',
-  jt: 24,
-  dzaybatsu: 100
-}
